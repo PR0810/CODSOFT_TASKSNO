3c65e4742c53877188187eea8885b8db8dc4ce7c
Prachi <prachisahu092006@gmail.com>
2026-07-19 11:10:13 +0530

    Fix merge conflicts - clean models and schemas

--- schemas.py
+++ schemas.py
@@ -1,9 +1,9 @@
+
 from flask_marshmallow import Marshmallow
 from marshmallow import fields, validate
 
 ma = Marshmallow()
 
-<<<<<<< HEAD
 class ContactSchema(ma.Schema):
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True, validate=validate.Length(min=1, max=100))
@@ -14,27 +14,3 @@ class ContactSchema(ma.Schema):
 
 contact_schema = ContactSchema()
 contacts_schema = ContactSchema(many=True)
-=======
-class StudentSchema(ma.Schema):
-    id = fields.Integer(dump_only=True)
-    name = fields.String(required=True, validate=validate.Length(min=1, max=100))
-    email = fields.Email(required=True)
-    age = fields.Integer(required=True, validate=validate.Range(min=1, max=100))
-
-class CourseSchema(ma.Schema):
-    id = fields.Integer(dump_only=True)
-    name = fields.String(required=True, validate=validate.Length(min=1, max=100))
-    duration = fields.String(required=True)
-
-class EnrollmentSchema(ma.Schema):
-    id = fields.Integer(dump_only=True)
-    student_id = fields.Integer(required=True)
-    course_id = fields.Integer(required=True)
-
-student_schema = StudentSchema()
-students_schema = StudentSchema(many=True)
-course_schema = CourseSchema()
-courses_schema = CourseSchema(many=True)
-enrollment_schema = EnrollmentSchema()
-enrollments_schema = EnrollmentSchema(many=True)
->>>>>>> 1c25ee110bd490b7b2298d3eb60cce8fef0bbeb2
